3a16597de57c9e06695741baaffccf17f22a8ed1
Agwaze Great <agwazegreat@gmail.com>
2025-10-02 16:05:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useReducer } from "react";
+import DateCounter from "./component/DateCounter";
+import Header from "./component/Header";
+import Main from "./Main";
+import Loader from "./component/Loader";
+import StartScreen from "./component/StartScreen";
+import Error from "./component/Error";
+import Questions from "./component/Questions";
+
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error','ready' , 'active', 'finished'
+  status: "loading",
+  index: 0,
+  answer: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+
+    case "newAnswer":
+      return {
+        ...state,
+        answer: action.payload,
+      };
+    default:
+      throw new Error("Action unknoen");
+  }
+}
+
+function App() {
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        {/* <p>1/15</p> 
+        <p>Question?</p> */}
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <Questions
+            questions={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
+      </Main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Questions.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Option from "./Option";
+
+export default function Questions({ questions, dispatch, answer }) {
+  return (
+    <div>
+      <h4>{questions.question}</h4>
+
+      <Option questions={questions} dispatch={dispatch} answer={answer} />
+    </div>
+  );
+}
